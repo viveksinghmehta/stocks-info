3185aeb52c5793ea2201c312cf202ae8218f1d27
Vivek Singh Mehta <viveksinghmehta.smr@gmail.com>
2025-08-08 23:49:32 +0530

    Add logging for WhatsApp stock query and Twilio send
    Introduced additional log statements in WhatsApp handler and Twilio service to aid debugging. Logs now capture query handling, stock search results, and Twilio phone number usage.

--- routes/whatsappHandler.go
+++ routes/whatsappHandler.go
@@ -40,8 +40,10 @@ func WhatsAppIncomingHandler(db *sql.DB) gin.HandlerFunc {
 
 		switch {
 		case strings.HasPrefix(body, "stock "):
+			log.Println("Handling Stock search query...")
 			handleStockQuery(db, phone, user, strings.TrimPrefix(body, "stock "), c)
 		case strings.HasPrefix(body, "alert "):
+			log.Println("Handling Stock alert query...")
 			handleStockAlerts(db, phone, user, strings.TrimPrefix(body, "alert "), c)
 		case body == "top stocks":
 			// TODO: implement top stocks logic
@@ -56,18 +58,25 @@ func WhatsAppIncomingHandler(db *sql.DB) gin.HandlerFunc {
 
 func handleStockQuery(db *sql.DB, phone string, user *model.User, query string, c *gin.Context) {
 	matches, err := services.SearchStocks(db, query)
+	log.Println("Matches :- ", matches)
 	if err != nil {
+		log.Println("Error :- ", err.Error())
 		c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
 		return
 	}
 
 	switch len(matches) {
 	case 0: // No stock font
+		log.Println("No stock found...")
 		msg := helper.NoStockFoundMessage()
 		services.SendWhatsApp(phone, msg)
 	case 1: // exact match found for the stock
+		log.Println(" Stock Symbol :- ", matches[0].Symbol)
+		log.Println(" Company Name :- ", matches[0].CompanyName)
 		stockPerformance, err := services.GetStockPerformance(matches[0].Symbol+".NS", matches[0].CompanyName)
+		log.Println("Stock Performance :- ", stockPerformance)
 		if err != nil {
+			log.Println("Failed to fetch stock price...")
 			c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to fetch stock price"})
 			return
 		}
--- services/twilio.go
+++ services/twilio.go
@@ -1,6 +1,7 @@
 package services
 
 import (
+	"log"
 	"os"
 	"stocks-info-channel/helper"
 
@@ -14,6 +15,7 @@ func SendWhatsApp(to, body string) (*openApi.ApiV2010Message, error) {
 		Password: os.Getenv(helper.EnvironmentConstant().TWILIO_AUTH_TOKEN),
 	})
 	params := &openApi.CreateMessageParams{}
+	log.Println(" Phone Number :- ", os.Getenv(helper.EnvironmentConstant().PHONE_NUMBER))
 	params.SetFrom(helper.AppConstant().WhatsApp + os.Getenv(helper.EnvironmentConstant().PHONE_NUMBER))
 	params.SetTo(helper.AppConstant().WhatsApp + to)
 	params.SetBody(body)
